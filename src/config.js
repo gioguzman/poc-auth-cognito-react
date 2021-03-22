3b101c85a8aeb6c0798c69960fc9f868a1c34398
unknown <g.guz.ces@gmail.com>
2021-03-21 19:36:01 -0500

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,9 @@
+const config = {
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_Qt3gRHQr8",
+      APP_CLIENT_ID: "45n0gm13ajb2ddkuglo6o6qpt1",
+    },
+  };
+  
+  export default config;
